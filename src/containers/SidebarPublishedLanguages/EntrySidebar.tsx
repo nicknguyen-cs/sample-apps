fdbcdf9a68e6ab4cb456709f2e80eabab530d796
nicknguyen-cs <nick.nguyen@contentstack.com>
2023-10-25 11:12:23 -0700

    Code cleanup

--- src/containers/SidebarPublishedLanguages/EntrySidebar.tsx
+++ src/containers/SidebarPublishedLanguages/EntrySidebar.tsx
@@ -1,67 +1,78 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import ContentstackAppSDK from "@contentstack/app-sdk";
 import "@contentstack/venus-components/build/main.css";
 import { Icon, InfiniteScrollTable } from '@contentstack/venus-components';
 import axios from 'axios';
-import { AppSDK } from "../../types/cloneTypes";
+
+interface Locale {
+  code: string;
+  localized?: boolean;
+}
+
+interface Entry {
+  publish_details: any[];
+  content_type: { uid: string };
+  _data: { uid: string };
+}
+
 
 function EntrySidebarExtension() {
-
   const [tableData, setTableData] = useState<any[]>([]);
   const [count, setCount] = useState<number>(0);
-  let [loading, setLoading] = useState<boolean>(true);
-  let [itemStatusMap, setItemStatusMap] = useState<any>([]);
-  let [config, setConfig] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [itemStatusMap, setItemStatusMap] = useState<any[]>([]);
 
   useEffect(() => {
-    ContentstackAppSDK.init().then(async (sdk) => {
-      var sidebarWidget = sdk.location.SidebarWidget;
-      var entry = await sidebarWidget?.entry;
-      let config = await sdk.getConfig();
-      let locales = await sdk.stack
-        .ContentType(entry.content_type.uid)
-        .Entry(entry._data.uid)
-        .getLanguages();
-      fetchData(entry._data, locales, config);
-    })
-  }, [])
+    initializeApp();
+  }, []);
 
-  async function fetchData(entry: any, locales: any, config: any) {
-    let data = await createTableData(entry, locales.locales, config);
+  const initializeApp = async () => {
+    const sdk = await ContentstackAppSDK.init();
+    const sidebarWidget = sdk.location.SidebarWidget;
+    const entry = await sidebarWidget?.entry;
+    const config = await sdk.getConfig();
+    const locales = await sdk.stack
+      .ContentType(entry.content_type.uid)
+      .Entry(entry._data.uid)
+      .getLanguages();
+    
+    fetchData(entry._data, locales.locales, config);
+  };
+
+  const fetchData = async (entry: Entry, locales: Locale[], config: any) => {
+    const data = await createTableData(entry, locales, config);
+    console.log("Data: ", data)
     setTableData(data);
     setLoading(false);
     setCount(data.length);
   }
 
+  const createTableData = async (entry: Entry, locales: Locale[], config: any) => {
+    if (!entry.publish_details) return [];
 
-  async function createTableData(entry: any, locales: any, config: any) {
-    console.log(locales);
-    var tableData: any[] = [];
-    if (entry.publish_details) {
-      tableData = entry.publish_details.map(async (data: any, index: number) => {
-        itemStatusMap[index] = 'loaded';
-        let name = await getEnvironmentName(data, config);
-        let matchingLocale = locales.find((item: any) => item.code === data.locale);
-        let icon;
-        if (matchingLocale && matchingLocale.localized) {
-          icon = <Icon icon='SuccessInverted' />;
-        }
-        return {
-          index: index,
-          env: name,
-          language: data.locale,
-          loc: icon
-        };
+    const tableDataPromises = entry.publish_details.map(async (data, index) => {
+      setItemStatusMap(prevMap => {
+        const updatedMap = [...prevMap];
+        updatedMap[index] = 'loaded';
+        return updatedMap;
+      });
 
-      })
-    }
-    tableData = await Promise.all(tableData);
-    console.log(tableData)
-    return tableData;
+      const name = await getEnvironmentName(data, config);
+      const matchingLocale = locales.find(locale => locale.code === data.locale);
+      const icon = matchingLocale?.localized ? <Icon icon='SuccessInverted' /> : null;
+      return {
+        index,
+        env: name,
+        language: data.locale,
+        loc: icon
+      };
+    });
+
+    return await Promise.all(tableDataPromises);
   }
 
-  async function getEnvironmentName(environment: any, config: any) {
-    var request = {
+  const getEnvironmentName = async (environment: any, config: any) => {
+    const request = {
       method: 'GET',
       url: 'https://api.contentstack.io/v3/environments',
       headers: {
@@ -71,31 +82,15 @@ function EntrySidebarExtension() {
       }
     };
 
-    const response = await axios(request)
-    for (let i = 0; i < response.data.environments.length; i++) {
-      let env = response.data.environments[i];
-      if (env.uid === environment.environment) {
-        return env.name;
-      }
-    }
+    const response = await axios(request);
+    const foundEnv = response.data.environments.find((env: any) => env.uid === environment.environment);
+    return foundEnv?.name;
   }
 
   const columns = [
-    {
-      Header: 'Language',
-      id: 'language',
-      accessor: "language",
-    },
-    {
-      Header: 'Environment',
-      id: "env",
-      accessor: "env",
-    },
-    {
-      Header: 'Localized',
-      id: "loc",
-      accessor: "loc",
-    }
+    { Header: 'Language', id: 'language', accessor: "language" },
+    { Header: 'Environment', id: "env", accessor: "env" },
+    { Header: 'Localized', id: "loc", accessor: "loc" }
   ]
 
   return (
