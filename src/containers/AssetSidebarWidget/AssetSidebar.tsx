b0031da93f0f910e8d28f054916245eac0dc5436
nicknguyen-cs <nick.nguyen@contentstack.com>
2024-05-21 09:51:32 -0700

    automate

--- src/containers/AssetSidebarWidget/AssetSidebar.tsx
+++ src/containers/AssetSidebarWidget/AssetSidebar.tsx
@@ -2,28 +2,36 @@ import Icon from "../../assets/assetsidebar.svg";
 import localeTexts from "../../common/locales/en-us/index";
 import parse from "html-react-parser";
 import ContentstackAppSDK from "@contentstack/app-sdk";
+import { useState, useEffect } from "react";
 
 const AssetSidebarExtension = () => {
-  ContentstackAppSDK.init().then(async function (appSdk) {
-    console.log(appSdk.locationUID);
-    let loc = appSdk.location.AssetSidebarWidget?.currentAsset._metadata;
-    console.log(loc);
-  })
+
+  const [scs, setScs] = useState("")
+  const [currentAsset, setCurrentAsset] = useState<any>()
+  const [locationUid, setLocationUid] = useState<any>()
+  const [metadataUid, setMetadataUid] = useState<any>()
+
+  useEffect(() => {
+    ContentstackAppSDK.init().then(async function (appSdk) {
+      let asset = appSdk.location.AssetSidebarWidget?.currentAsset
+      setCurrentAsset(asset);
+      setLocationUid(appSdk.locationUID)
+      if (asset?._metadata) {
+        setScs(asset?._metadata.extensions[appSdk.locationUID][0].demo_metadata)
+        setMetadataUid(asset?._metadata.extensions[appSdk.locationUID][0].uid);
+      } 
+    })
+  }, [])
+
   return (
     <div className="asset-sidebar">
       <div className="asset-sidebar-container">
-        <div className="asset-sidebar-icon">
-          <img src={Icon} alt="icon" />
-        </div>
-        <div className="app-component-content">
-          <h4>{localeTexts.AssetSidebarWidget.title}</h4>
-          <p>{parse(localeTexts.AssetSidebarWidget.body)}</p>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://www.contentstack.com/docs/developers/developer-hub/asset-sidebar-location/">
-            {localeTexts.AssetSidebarWidget.button.learnMore}
-          </a>
+        <div className="asset-sidebar-icon" style={{textAlign: "left"}}>
+          Extension UID: {locationUid}
+          <br />
+          Metadata UID: {metadataUid}
+          <br/>
+          Metadata: {scs}
         </div>
       </div>
     </div>
